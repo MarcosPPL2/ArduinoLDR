5caf025f5f9bc8e0f4344904408fdf6c4fb1d9f7
MarcosPauloPL <106969856+MarcosPauloPL@users.noreply.github.com>
2022-06-06 09:04:24 -0300

    Add files via upload

---
+++ sensorV2.1.ino
@@ -0,0 +1,33 @@
+int valorldr = 0;
+int pinoLed = 3;
+float valorPO = 0;
+int temp = 0;
+void setup() {   
+   pinMode(A1, INPUT); // ENTRADA ANALOGICA LDR
+   Serial.begin(9600);
+   pinMode(3, OUTPUT); // SAIDA LED DIGITAL
+   pinMode(A4, INPUT); // ENTRADA ANALOGICA POTENCIOMETRO 
+  }
+void loop(){
+    valorPO = analogRead(A4); // LE O VALOR DO POTENCIOMETRO
+    valorldr = analogRead(A1); // LE O VALOR DO LDR
+    valorldr = constrain(valorldr, 0, 200); // DELIMITA OS VALORES DO LDR
+    valorPO = constrain(valorPO, 0, 200); // DELIMITA OS VALORES DO POTENCIOMETRO
+    valorldr = map(valorldr, 0,200,200,0); // INVERTE OS VALORES DO LDR PARA ASCENDER A LED QUANDO ESCURO
+    analogWrite(pinoLed, valorldr); // PASSA O VALOR LDR PARA O LED 
+    Serial.println(valorPO); // MOSTRA O VALOR DO POTENCIOMETRO
+    delay(200);
+    temp = valorPO * 30; // VARIAVEL PARA DEFINIR O DELAY CONFORME O VALOR LIDO NO POTENCIOMETRO
+    if (valorldr > 150) {
+      delay(temp); 
+      
+      }
+    
+    
+    
+    
+    
+    }
+  
+
+  
